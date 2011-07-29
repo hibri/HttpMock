77f4393c5065d0e7fe7e7cc4106463a2fbffeab7
Greg Sochanik <greg.sochanik@7digital.com>
2011-07-29 13:29:58 +0100

    Added tests around all httpmethods on the same stub

--- src/HttpMock.Integration.Tests/MultipleTestsUsingTheSameStubServerAndDifferentUris.cs
+++ src/HttpMock.Integration.Tests/MultipleTestsUsingTheSameStubServerAndDifferentUris.cs
@@ -1,92 +1,10 @@
 ﻿using System;
-using System.IO;
 using System.Net;
 using HttpMock;
 using NUnit.Framework;
 
 namespace SevenDigital.HttpMock.Integration.Tests
 {
-	[TestFixture]
-	public class MultipleTestsUsingTheSameStubServerWithDifferentHttpMethods
-	{
-		private const string ENDPOINT_TO_HIT = "http://localhost:8080/endpoint";
-		private IStubHttp _httpMockRepository;
-
-		[TestFixtureSetUp]
-		public void SetUp() {
-			_httpMockRepository = HttpMockRepository.At("http://localhost:8080/");
-
-			_httpMockRepository
-				.Stub(x => x.Get("/endpoint"))
-				.Return("I am a GET")
-				.OK();
-
-			_httpMockRepository
-				.Stub(x => x.Post("/endpoint"))
-				.Return("I am a POST")
-				.OK();
-
-			_httpMockRepository
-				.Stub(x => x.Put("/endpoint"))
-				.Return("I am a PUT")
-				.OK();
-
-			_httpMockRepository
-				.Stub(x => x.Delete("/endpoint"))
-				.Return("I am a DELETE")
-				.OK();
-
-			_httpMockRepository
-				.Stub(x => x.Head("/endpoint"))
-				.Return("I am a HEAD")
-				.OK();
-
-		}
-
-		[Test]
-		public void Should_get() {
-			AssertResponse("GET", "I am a GET");
-		}
-
-		[Test]
-		public void Should_post() {
-			AssertResponse("POST", "I am a POST");
-		}
-
-		[Test]
-		public void Should_put() {
-			AssertResponse("PUT", "I am a PUT");
-		}
-
-		[Test]
-		public void Should_delete() {
-			AssertResponse("DELETE", "I am a DELETE");
-		}
-
-		[Test]
-		public void Should_head() {
-			var webRequest = (HttpWebRequest)WebRequest.Create(ENDPOINT_TO_HIT);
-			webRequest.Method = "HEAD";
-			using (var response = webRequest.GetResponse()) { 
-				
-			}
-		}
-
-		private void AssertResponse(string method, string expected) {
-
-			var webRequest = (HttpWebRequest)WebRequest.Create(ENDPOINT_TO_HIT);
-			webRequest.Method = method;
-			using(var response = webRequest.GetResponse()) {
-				using(var sr = new StreamReader(response.GetResponseStream()))
-				{
-					string readToEnd = sr.ReadToEnd();
-					Assert.That(readToEnd, Is.EqualTo(expected));
-				}
-			}
-		}
-	}
-
-
 	[TestFixture]
 	public class MultipleTestsUsingTheSameStubServerAndDifferentUris
 	{
