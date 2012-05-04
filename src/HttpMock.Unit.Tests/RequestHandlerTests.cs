c81bd23008ec4e50cdc8e5f0008f8266d8cb6fe7
Nick <nick@ntcoding.co.uk>
2012-05-04 13:47:42 +0100

    Request handler applies constraints against urls

---
+++ src/HttpMock.Unit.Tests/RequestHandlerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace HttpMock.Unit.Tests
+{
+    [TestFixture]
+    public class RequestHandlerTests
+    {
+        [Test]
+        public void Applies_One_Constraint()
+        {
+            var doesNotContainBlahConstraint = new Func<Uri, bool>(uri => uri.AbsoluteUri.Contains("blah") == false);
+
+            var h = new RequestHandler("", null);
+
+            h.WithUrlConstraint(doesNotContainBlahConstraint);
+
+            var uriWithBlah = new Uri("http://www.blah.com");
+
+            Assert.That(h.CanVerifyConstraintsFor(uriWithBlah), Is.EqualTo(false));
+        }
+
+        // applies multiple constraints 
+    }
+}
