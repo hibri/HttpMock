348f519f8c1e6fddacb26cc2228b0410c6b09017
Andres G. Aragoneses <andres@7digital.com>
2012-11-21 18:24:14 +0000

    HttpServer: Fix IsAvailable() to be able to return FALSE
    If the method .Start() of HttpServer was not called, IsAvailable() was
    throwing an exception instead of returning FALSE.
    
    This change has the added benefit that improves HttpMock a lot when run
    with Mono. Why? Because apparently the _thread.Start(); in Microsoft.NET
    is very fast and IsAvailable always was queried AFTER the server thread
    had already started Kayak. In Mono, this fast is not so fast, so then
    IsAvailable was throwing the exception and not using the retrying loop
    (that is already in place) properly.
    
    This is not a Mono bug. When using multi-threading code, you should never
    assume any certain speed of any thread.

---
+++ src/HttpMock.Unit.Tests/HttpServerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HttpMock;
+using NUnit.Framework;
+
+namespace SevenDigital.HttpMock.Unit.Tests
+{
+	[TestFixture]
+	public class HttpServerTests
+	{
+		[Test]
+		public void IsAvailableReturnsFalseIfStartNotCalled()
+		{
+			IHttpServer httpServer = new HttpServer(new Uri("http://localhost:9099"));
+			Assert.That(httpServer.IsAvailable(), Is.EqualTo(false));
+		}
+	}
+}
